7982f4e006583199e7666f7b53b2ea91da701361
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-03-14 21:25:17 +0400

    fix package.json

--- src/components/CelebrationButton/СelebrationButton.jsx
+++ src/components/CelebrationButton/СelebrationButton.jsx
@@ -1,7 +1,10 @@
 /* eslint-disable import/no-unresolved */
 import { React, useCallback } from 'react';
 import './CelebrationButton.scss';
-import { confetti } from 'https://cdn.skypack.dev/canvas-confetti@1';
+// import {confetti} from 'https://cdn.skypack.dev/canvas-confetti@1';
+
+// eslint-disable-next-line import/no-extraneous-dependencies
+import confetti from 'canvas-confetti';
 
 function CelebrationButton({ buttonText, handleCelebrationButton }) {
   const addedConfetti = useCallback(() => {
