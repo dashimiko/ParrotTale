67477b2d950c2cf83c39ce395150d5383f1272c7
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-03-24 14:40:51 +0400

    added strict mode

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import App from './components/App/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  // <React.StrictMode>
   <BrowserRouter>
-    <App />
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
   </BrowserRouter>
-  // </React.StrictMode>
 );
