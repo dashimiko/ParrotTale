82bccba15c9df24eb48b7aaa65a59dd52e902bec
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-04-08 17:34:40 +0400

    parrot animation

--- src/components/NotFound/NotFound.jsx
+++ src/components/NotFound/NotFound.jsx
@@ -10,10 +10,17 @@ function NotFound() {
       <div className="container">
         <div className="loro">
           <div className="circ" />
+          <div className="circ8" />
           <div className="circ3" />
           <div className="circ5" />
           <div className="circ7" />
           <div className="ojo" />
+          <div className="detail" />
+          <div className="text-figure">
+            <p className="aaaa">
+              AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA
+            </p>
+          </div>
         </div>
         <div />
       </div>
