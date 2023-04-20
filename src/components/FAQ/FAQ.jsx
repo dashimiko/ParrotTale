256f2641ada46edebd3b5325eb53d3124fd987d6
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-04-20 10:47:04 +0400

    fix FAQ block

--- src/components/FAQ/FAQ.jsx
+++ src/components/FAQ/FAQ.jsx
@@ -7,34 +7,53 @@ function FAQ() {
         <h2 className="FAQ__title">Frequently Asked Questions</h2>
         <details className="FAQ__accordion-box">
           <summary className="FAQ__question">
-            How do I create accordion?
+            How many species of parrots are there?
           </summary>
           <div className="FAQ__answer">
-            The tags <em>details</em> and <em>summary</em> have you covered.
+            There are <em>around 393 species</em> of parrots that have been
+            recognized by the International Union for Conservation of Nature
+            (IUCN). However, the exact number of species is still a matter of
+            debate among experts, as some parrot species have multiple
+            subspecies and the taxonomy of some groups of parrots is still being
+            studied.
           </div>
         </details>
         <details className="FAQ__accordion-box FAQ__accordion-box_2">
           <summary className="FAQ__question">
-            How do I create accordion?
+            Why did the kakapo parrot almost become extinct in New Zealand?
           </summary>
           <div className="FAQ__answer">
-            The tags <em>details</em> and <em>summary</em> have you covered.
+            The kakapo parrot almost became extinct in New Zealand due to a
+            combination of factors such as habitat loss, hunting by humans, and
+            the introduction of predators like rats, stoats, and cats. The birds
+            are flightless and have a slow reproductive rate, making them
+            vulnerable to these threats. Conservation efforts have been
+            implemented to protect and increase the population of the kakapo,
+            including the creation of predator-free islands and a captive
+            breeding program.
           </div>
         </details>
         <details className="FAQ__accordion-box FAQ__accordion-box_3">
           <summary className="FAQ__question">
-            How do I create accordion?
+            How intelligent are parrots?
           </summary>
           <div className="FAQ__answer">
-            The tags <em>details</em> and <em>summary</em> have you covered.
+            Parrots are known for their <em>high level of intelligences</em>{' '}
+            among birds. They can be trained to mimic human speech and can
+            understand and use language to communicate with their owners. They
+            also have the ability to solve problems, demonstrate empathy, and
+            exhibit a wide range of emotions. Some parrot species have been
+            shown to possess cognitive abilities that are comparable to those of
+            great apes and dolphins.
           </div>
         </details>
         <details className="FAQ__accordion-box FAQ__accordion-box_4">
           <summary className="FAQ__question">
-            How do I create accordion?
+            What are the most common types of parrots in the world?
           </summary>
           <div className="FAQ__answer">
-            The tags <em>details</em> and <em>summary</em> have you covered.
+            The most common types of parrots in the world are budgerigars,
+            cockatiels, Amazon parrots, African grey parrots, and macaws.
           </div>
         </details>
       </div>
