d5617be592e6a5037156e20a1af9441899306174
Daria Mikolaichuk <dashamikolaichuk@gmail.com>
2023-04-24 16:45:49 +0400

    started adding adaptive in promo block

--- src/components/Promo/Promo.jsx
+++ src/components/Promo/Promo.jsx
@@ -10,9 +10,17 @@ function Promo() {
         <div className="promo__title-box">
           <h1 className="promo__title">
             Combine
-            <span className="promo__strong-word"> fine</span> pictures
+            <span className="promo__strong-word_desktop">
+              <span className="promo__strong-word"> fine</span> pictures
+            </span>
+            <span className="promo__strong-word_mobile">
+              <span className="promo__strong-word"> cool</span> images
+            </span>
           </h1>
-          <h2 className="promo__subtitle">To represent a rainbow</h2>
+          <h2 className="promo__subtitle_desktop">To represent a rainbow</h2>
+          <h2 className="promo__subtitle_mobile">
+            To create any grid by scaling and resizing
+          </h2>
         </div>
         <PictureBox />
       </div>
